68463b052bba9204ef27492ec86ad792f58f02fc
陳新(Hsin Chen) <1909002@iisigroup.com>
2022-05-10 19:35:07 +0800

    修改 provider.tf

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -1,5 +1,19 @@
 # Initialises Terraform providers and sets their version numbers.
 
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "1.38.0"
+  features {}
+
+  subscription_id = "02b1df81-a557-4706-b3b6-e3e1242251e6"
 }
